8fb37a3392df009da51e98457e3fdbe17150f89d
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-29 20:46:11 +0900

    rename: HelmetComponent -> Helmets

---
+++ src/Helmets.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+export function Helmets() {
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Electrolize&family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+    </HelmetProvider>
+  );
+}
