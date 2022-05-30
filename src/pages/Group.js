86639b8388cf7c5fc48216df4e16d43d4e1e9b91
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-05-30 14:15:18 +0900

    design: Group-list UI

---
+++ src/pages/Group.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Category, Filter } from "../components/group/index";
+
+function Group() {
+  return (
+    <>
+      <Category />
+      <Filter />
+    </>
+  );
+}
+
+export default Group;
