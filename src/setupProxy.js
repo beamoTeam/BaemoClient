43206f8e5a95b9b6d266a667d6ea501029b79a9e
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-27 23:23:54 +0900

    chage baseURL

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
-  // app.use(
-  //   '/oauth',
-  //   createProxyMiddleware({
-  //     target: "http://3.94.44.116:2999",
-  //     changeOrigin: true,
-  //   }))
   app.use(
     '/api',
     createProxyMiddleware({
