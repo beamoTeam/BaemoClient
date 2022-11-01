a585dc7f235e5fdcd4698af21c17998c50ce133b
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-01 11:10:59 +0900

    save

--- src/pages/MenuDetail.tsx
+++ src/pages/MenuDetail.tsx
@@ -13,7 +13,6 @@ import { useEffect, useState } from "react";
 import restaurantService from "../lib/api/RestaurantService";
 import groupOrderService from "../lib/api/GroupOrderService";
 import { MenuModel } from "../types/menu";
-import css from "./MenuDetail.css";
 import { useHistory, useLocation } from "react-router";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { useCartState } from "../lib/recoil/cartState";
