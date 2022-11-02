ea72cc3cb8d577287d5f3ebe8b9c1d493506194c
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-02 13:53:38 +0900

    save

--- src/components/button/FloatChatButton.tsx
+++ src/components/button/FloatChatButton.tsx
@@ -4,29 +4,17 @@ import { addOutline, chatbubblesOutline, cartOutline } from "ionicons/icons";
 import css from "./FloatCartButton.module.css";
 
 export default function FloatChatButton() {
-  // return (
-  //   <div onClick={() => console.log("채팅 버튼 클릭")}>
-  //     <div className={css.cartPosition}>
-  //       <IonFab vertical="bottom" horizontal="start" slot="fixed">
-  //         <IonFabButton>
-  //           <IonIcon icon={cartOutline} />
-  //           <div className={css.counter}>{}</div>
-  //         </IonFabButton>
-  //       </IonFab>
-  //     </div>
-  //   </div>
-  // );
   return (
-    <div className={css.chatPosition}>
+    <div className={css.cartPosition}>
       <IonFab vertical="bottom" horizontal="start" edge slot="fixed">
         <IonFabButton>
           <IonIcon icon={addOutline} />
         </IonFabButton>
         <IonFabList side="top">
-          <IonFabButton>
+          <IonFabButton color="dark">
             <IonIcon icon={cartOutline} />
           </IonFabButton>
-          <IonFabButton>
+          <IonFabButton color="dark">
             <IonIcon icon={chatbubblesOutline} />
           </IonFabButton>
         </IonFabList>
