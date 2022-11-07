bd1cf064e3e7b10e2372fcac1fe94bd2fc946445
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-07 23:31:37 +0900

    save

--- src/components/modal/SlideMenu.tsx
+++ src/components/modal/SlideMenu.tsx
@@ -27,7 +27,7 @@ export default function SlideMenu({ close }: SlideMenuProps) {
 
   const parsedMenus = Object.keys(parsedChatMenu).map((sender: any) => ({
     sender,
-    menu: parseMenu(parsedChatMenu[sender]),
+    menu: parsedChatMenu[sender],
   }));
   console.log(parsedMenus);
 
