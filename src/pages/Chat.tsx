bc8055574f7e01495d04f7a7589eabf2e05dddfa
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-07 22:28:38 +0900

    save

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -130,7 +130,7 @@ export default function Chat() {
             msgList.map((message: any) => (
               <div key={message.id}>
                 {message.date && <DateIndicator date={message.date} />}
-                {message.menu && <LeftChatBox menu={message.menu} />}
+                {/* {message.menu && <LeftChatBox message={message} />} */}
                 <MenuBox menu={message.menu} />
                 <div className={css.textBox}>
                   {message.sender !== sender && (
