ba42e086e57b271786fafe0c618628638c502de2
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-16 11:15:10 +0900

    save

--- src/components/modal/AddressModal.tsx
+++ src/components/modal/AddressModal.tsx
@@ -40,20 +40,16 @@ function AddressModal() {
 
   return (
     <div>
-      <div>
       <IonModal
         isOpen={present}
         initialBreakpoint={0.6}
         breakpoints={[0, 0.25, 0.5, 0.65]}
         onDidDismiss={onDidDismiss}
       >
-          <div style={{ marginTop: "15px", height: "100%" }}>
-
-        <DaumPostcodeEmbed onComplete={onComplete} />
-          </div>
+        <div style={{ marginTop: "15px", height: "100%" }}>
+          <DaumPostcodeEmbed onComplete={onComplete} />
+        </div>
       </IonModal>
-    </div>
-
     </div>
   );
 }
